1903c65d106376e950ae95e112b9afa92154a921
alis70929 <60583162+alis70929@users.noreply.github.com>
2020-02-27 12:05:20 +1300

    Rounds system finished

--- HL_Component3_Rounds.py
+++ HL_Component3_Rounds.py
@@ -4,3 +4,10 @@
 #  - loop for amount of rounds
 #  - store amount of rounds played as a variable so it can be displayed later
 
+rounds = int(input("How many rounds?: "))
+rounds_played = 0
+
+while rounds_played < rounds:
+    rounds_played += 1
+    print("Rounds played: {}".format(rounds_played))
+print("End of game")
