362898a4e8e6ae65e3700820dcd6891814b90017
alis70929 <60583162+alis70929@users.noreply.github.com>
2020-03-05 11:41:15 +1300

    Update HL_CompleteProgram.py

--- HL_CompleteProgram.py
+++ HL_CompleteProgram.py
@@ -16,7 +16,6 @@ def intcheck(question,low = None,high = None):
     else: # Error message if no variables are given
         error = "please enter a whole number"
 
-
     while not valid:
         try:
             # Gets user input
@@ -36,25 +35,46 @@ def intcheck(question,low = None,high = None):
             print(error)
             print()
 
-def GuessCalc(high,low):
+
+def guess_calc(high,low):
     range = high - low + 1
     max_raw = math.log2(range)  # gets maximum number of guesses using binary search
     max_upped = math.ceil(max_raw)  # Rounds up max_raw
     max_guesses = max_upped + 1  # Gives the user an extra guess incase they mess up
     return max_guesses
 
+
+
+
 #Main routine goes here
-rounds = intcheck("How many rounds do you want to play?: ",1)# Asks how many rounds user wants to play
+rounds = intcheck("How many rounds do you want to play?: ",1)  # Asks how many rounds user wants to play
 rounds_played = 0
 
 while rounds_played < rounds:
-    rounds_played += 1 # This round has started so add 1 to how many rounds are played
-    print("Rounds played: {}".format(rounds_played)) # Display how many rounds played to the user
-    low_num = intcheck("What do you want the lowest number to be: ") # Gets lowest number from the user
-    high_num = intcheck("What do you want the highest number to be: ".format(low_num),low_num) # Gets the highest number from the user
-    max_guesses = GuessCalc(high_num,low_num)
-    random_num = random.randint(low_num, high_num)  # Generates random number
-    guess = intcheck("What is your guess: ",low_num,high_num) # Gets user guess between low number and high number
+    rounds_played += 1  # This round has started so add 1 to how many rounds are played
+    guesses = 0  # Reset the amount of guessses at the start of a new round
+    print("Round {}".format(rounds_played)) # Display what round the user is on
+
+    low_num = intcheck("What do you want the lowest number to be: ")  # Gets lowest number from the user
+    high_num = intcheck("What do you want the highest number to be: ".format(low_num),low_num)  # Gets the highest number from the user
+
+    max_guesses = guess_calc(high_num,low_num)
+    secret_num = random.randint(low_num, high_num)  # Generates random number
+    while guesses <= max_guesses:
+        guess = intcheck("What is your guess: ",low_num,high_num)  # Gets user guess between low number and high number
+        guesses += 1
+        # Compares users guess to the secret number and gives appropriate feedback
+        if guess < secret_num:  # If guess is lower than secret number tell user to go higher
+            print("Higher")
+        elif guess > secret_num:  # If guess is higher than secret number tell user to go lower
+            print("Lower")
+        elif guess == secret_num:  # if user gets it right then congratulate them
+            print("Congratulations, You got it right")
+            guesses = max_guesses + 1
+
+
+
+
 
 
 
