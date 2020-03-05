259ef7633b3f2f5fb8e67dff90d3d57320558d8b
alis70929 <60583162+alis70929@users.noreply.github.com>
2020-03-06 10:51:01 +1300

    Finished component 8

--- HL_Component8_CantGuessTwice.py
+++ HL_Component8_CantGuessTwice.py
@@ -1,13 +1,19 @@
-# HL Component 6 - Comparing the users guess to the secret number
+# HL Component 8 - Makes sure that the user cant guess the same thing twice
 
 correct_guess = False
 
 max_guesses = int(input("Max guesses: "))
 guesses = 0
+already_guessed = []
 while guesses < max_guesses:
     guess = int(input("Guess: "))
+    if guess in already_guessed:
+        print("You have guessed this number already, please try again")
+        continue
     guesses += 1
+    already_guessed.append(guess)
     guesses_left = max_guesses - guesses
+
     if guesses_left >= 1:
         print("You have {} guesses left".format(guesses_left))
     else:
