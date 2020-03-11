2b28755043bf6688173b5d7804cefecfebad4fb6
alis70929 <60583162+alis70929@users.noreply.github.com>
2020-03-11 13:11:06 +1300

    Updated completed program
    Changed V1 to include how many components it has in its name.
    
    Changed v2 to include component 9 and got rid of som more useless variables

--- HL_CompleteProgramV2.py
+++ HL_CompleteProgramV2.py
@@ -48,14 +48,12 @@ def guess_calc(high,low):
 
 #Main routine goes here
 rounds = intcheck("How many rounds do you want to play?: ",1)  # Asks how many rounds user wants to play
-rounds_played = 0
-
+rounds_played = 0  # Counts rounds played
+num_won = 0  # Counts rounds won
 while rounds_played < rounds:
-    print() # seperates each round
+    print() # separates each round
 
     rounds_played += 1  # This round has started so add 1 to how many rounds are played
-    guesses = 0  # Reset the amount of guessses at the start of a new round
-    correct_guess = False # Resets the correct guess indicator
     already_guessed = [] # List that contains all guesses in this round
 
     print("Round {}".format(rounds_played)) # Display what round the user is on
@@ -65,13 +63,13 @@ while rounds_played < rounds:
     low_num = intcheck("What do you want the lowest number to be: ")  # Gets lowest number from the user
     high_num = intcheck("What do you want the highest number to be: ".format(low_num),low_num)  # Gets the highest number from the user
 
-    max_guesses = guess_calc(high_num,low_num) # Generates the max number of guesses
+    max_guesses = guess_calc(high_num,low_num)  # Generates the max number of guesses
     secret_num = random.randint(low_num, high_num)  # Generates random number
-
-    print("You have a max of {} guesses".format(max_guesses))  # Tells user their max amount of guesses
+    guesses_left = max_guesses # Sets amount of guesses fo the user
+    print("You have a max of {} guesses".format(guesses_left))  # Tells user their max amount of guesses
 
     # loops question until user runs out of guess or gets it right
-    while guesses < max_guesses and secret_num != guess:
+    while guesses_left >= 1 and secret_num != guess:
         guess = intcheck("What is your guess: ",low_num,high_num)  # Gets user guess between low number and high number
 
         # Checks if user has guessed the number before if the user has it loops the question.
@@ -80,25 +78,24 @@ while rounds_played < rounds:
             print("You have {} guesses left".format(guesses_left))
             continue
 
-        guesses += 1
-        guesses_left = max_guesses - guesses
-        already_guessed.append(guess) # Adds the current guess to the list of already_guessed above
+        guesses_left -= 1
+        already_guessed.append(guess)  # Adds the current guess to the list of already_guessed above
 
-        if guesses_left >= 1: # Compares users guess to the secret number and gives appropriate feedback
+        if guesses_left >= 1:  # Compares users guess to the secret number and gives appropriate feedback
             if guess < secret_num:  # If guess is lower than secret number tell user to go higher
-                print("Higher") # Feedback
+                print("Higher")  # Feedback
             elif guess > secret_num:  # If guess is higher than secret number tell user to go lower
-                print("Lower") # Feedback
+                print("Lower")  # Feedback
             elif guess == secret_num:  # if user gets it right then congratulate them
-                print("Congratulations, You got it right in {} guesses".format(guesses))
-
-
-
-        # Prints appropiate feedback based on the amount of guessses the user has left
+                print("Congratulations, You got it right in {} guesses".format(max_guesses - guesses_left))
+                num_won += 1
+        # Prints appropiate feedback based on the amount of guesses the user has left
         if guesses_left >= 1 and guess != secret_num:
             print("You have {} guesses left".format(guesses_left))
         elif guess != secret_num :
             print("you ran out of guesses")
+    print("Won: {} | Lost: {}".format(num_won, rounds_played - num_won))
+
 
 
 
