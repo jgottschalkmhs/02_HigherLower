6068aa7c3920c7359a15164b1eb7d6b37b44102c
alis70929 <60583162+alis70929@users.noreply.github.com>
2020-03-13 10:55:20 +1300

    Got rid of annoying little errors in complete program

--- HL_CompleteProgramV2.py
+++ HL_CompleteProgramV2.py
@@ -1,19 +1,28 @@
 # HL Complete program
 # Im putting together the code as i make the components
+import math
+import random
 
-import random, math
 
-#Functions go here
-def intcheck(question,low = None,high = None):
+# Functions go here
+def hl_statement(statement, char):
+    print()
+    print(char*len(statement))
+    print(statement)
+    print(char * len(statement))
+    print()
+
+
+def intcheck(question, low=None, high=None):
     valid = False
     # Error messages
-    if low is not None and high is not None: # Error message if variables are given
-        error = "Please enter a whole number between {} and {} (Inclusive) ".format(low,high)
-    elif low is not None and high is None: # Error message if only low variable is given
+    if low is not None and high is not None:  # Error message if variables are given
+        error = "Please enter a whole number between {} and {} (Inclusive) ".format(low, high)
+    elif low is not None and high is None:  # Error message if only low variable is given
         error = "Please enter a whole number equal to or above {} ".format(low)
-    elif low is not None and high is None: # Error message if only high variable is given
+    elif low is not None and high is None:  # Error message if only high variable is given
         error = "Please enter a whole number equal to or below {} ".format(high)
-    else: # Error message if no variables are given
+    else:  # Error message if no variables are given
         error = "please enter a whole number"
 
     while not valid:
@@ -22,11 +31,11 @@ def intcheck(question,low = None,high = None):
             response = int(input(question.format(low, high)))
             # Checks number is not too low
             if low is not None and response < low:
-                print(error) # If its too low  display error
+                print(error)  # If its too low  display error
                 continue
             # Checks number is not too high
             if high is not None and response > high:
-                print(error) # If it is too high print error
+                print(error)  # If it is too high print error
                 continue
 
             return response
@@ -36,41 +45,41 @@ def intcheck(question,low = None,high = None):
             print()
 
 
-def guess_calc(high,low):
-    range = high - low + 1
-    max_raw = math.log2(range)  # gets maximum number of guesses using binary search
+def guess_calc(high, low):
+    given_range = high - low + 1
+    max_raw = math.log2(given_range)  # gets maximum number of guesses using binary search
     max_upped = math.ceil(max_raw)  # Rounds up max_raw
     max_guesses = max_upped + 1  # Gives the user an extra guess incase they mess up
     return max_guesses
 
 
-
-
-#Main routine goes here
-rounds = intcheck("How many rounds do you want to play?: ",1)  # Asks how many rounds user wants to play
+# Main routine goes here
+rounds = intcheck("How many rounds do you want to play?: ", 1)  # Asks how many rounds user wants to play
 rounds_played = 0  # Counts rounds played
 num_won = 0  # Counts rounds won
 while rounds_played < rounds:
-    print() # separates each round
+    print()  # separates each round
 
     rounds_played += 1  # This round has started so add 1 to how many rounds are played
-    already_guessed = [] # List that contains all guesses in this round
+    already_guessed = []  # List that contains all guesses in this round
 
-    print("Round {}".format(rounds_played)) # Display what round the user is on
+    print("Round {}".format(rounds_played))  # Display what round the user is on
 
     guess = ""
     # Gets range for secret number from the user
     low_num = intcheck("What do you want the lowest number to be: ")  # Gets lowest number from the user
-    high_num = intcheck("What do you want the highest number to be: ".format(low_num),low_num)  # Gets the highest number from the user
+    # Gets the highest number from the user
+    high_num = intcheck("What do you want the highest number to be: ".format(low_num), low_num)
 
-    max_guesses = guess_calc(high_num,low_num)  # Generates the max number of guesses
+    maximum_guesses = guess_calc(high_num, low_num)  # Generates the max number of guesses
     secret_num = random.randint(low_num, high_num)  # Generates random number
-    guesses_left = max_guesses # Sets amount of guesses fo the user
+    guesses_left = maximum_guesses  # Sets amount of guesses fo the user
     print("You have a max of {} guesses".format(guesses_left))  # Tells user their max amount of guesses
 
     # loops question until user runs out of guess or gets it right
     while guesses_left >= 1 and secret_num != guess:
-        guess = intcheck("What is your guess: ",low_num,high_num)  # Gets user guess between low number and high number
+        # Gets user guess between low number and high number
+        guess = intcheck("What is your guess: ", low_num, high_num)
 
         # Checks if user has guessed the number before if the user has it loops the question.
         if guess in already_guessed:
@@ -87,27 +96,14 @@ while rounds_played < rounds:
             elif guess > secret_num:  # If guess is higher than secret number tell user to go lower
                 print("Lower")  # Feedback
             elif guess == secret_num:  # if user gets it right then congratulate them
-                if guesses_left == max_guesses - 1:
+                if guesses_left == maximum_guesses - 1:
                     print("Amazing!, You got it right in 1 guess")
                 else:
-                    print("Congratulations, You got it right in {} guesses".format(max_guesses - guesses_left))
+                    print("Congratulations, You got it right in {} guesses".format(maximum_guesses - guesses_left))
                 num_won += 1
         # Prints appropiate feedback based on the amount of guesses the user has left
         if guesses_left >= 1 and guess != secret_num:
             print("You have {} guesses left".format(guesses_left))
-        elif guess != secret_num :
+        elif guess != secret_num:
             print("you ran out of guesses")
     print("Won: {} | Lost: {}".format(num_won, rounds_played - num_won))
-
-
-
-
-
-
-
-
-
-
-
-
-
