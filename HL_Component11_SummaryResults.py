20605024b21066dfa206faf2865fb8d60a1643af
alis70929 <60583162+alis70929@users.noreply.github.com>
2020-03-18 12:32:57 +1300

    Started component 11

---
+++ HL_Component11_SummaryResults.py
@@ -0,0 +1,7 @@
+# Summary Results
+
+# Get results in each round and store in a list
+# Display the score from each round and if they won the round or not
+# Also show the users best round score , worst round score, and average score
+
+# For this one hope to look for different ways to do it
