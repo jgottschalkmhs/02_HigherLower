21f4007fdd783e46096f87552eeb6361a737af42
alis70929 <60583162+alis70929@users.noreply.github.com>
2020-03-25 12:24:56 +1300

    Component 12 looping game done

--- HL_Component12_EndGameLoop.py
+++ HL_Component12_EndGameLoop.py
@@ -1,3 +1,9 @@
 # End game loop
 
-# ask user if they want to
+# ask user if they want to play again
+# If they press enter game loops in its entirety
+# If they press any other key the game finishes
+
+keepgoing = ""
+while keepgoing == "":
+    keepgoing = input("Press <enter> to play again, Press any other key then <enter> to finish")
