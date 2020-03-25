a03084af42a9eaa18bbb3d67f5cdf422d2589041
alis70929 <60583162+alis70929@users.noreply.github.com>
2020-03-25 13:10:57 +1300

    Introduction done ish

---
+++ HL_Component1_Intro.py
@@ -0,0 +1,11 @@
+# Introduction that introduces the user to the game
+
+# I Will try my best to make it understandable
+
+print("---------------------- Welcome to Higher Lower ----------------------")
+print("In this higher lower game you will choose two numbers.")
+print("The computer will think of a number between you're chosen numbers.")
+print("You will attempt to guess the number that the computer is thinking of.")
+print("You will be given enough guesses to guess the number.")
+print("You can choose how many rounds you want to play.")
+print("At the end of the game the game will give you a summary on how you did.")
